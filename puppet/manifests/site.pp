1b3736f0db8a817073046045ea37d71bd7b00939
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-04-13 01:28:07 +0100

    Attempt to configure hosts via hiera
    Start by adding configuration that pulls information out of hiera, need
    to add vagrant magic to dump the actual config itself onto the machines
    
    Change-Id: Ieb48f1f07a0fb59d5cc2e493c901dc3fae8925a4

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,10 @@
+$newbury_nic = hiera('newbury_nic')
+$floor_nic = hiera('floor_nic')
+$newbury_side_comp_addr = hiera('newbury_side_comp_addr')
+$floor_comp_addr = hiera('floor_comp_addr')
+$floor_comp_net = hiera('floor_comp_net')
+$lease_range = hiera('lease_range')
+
 file { '/etc/sr_network_setup.sh':
   content => template('sr_network_setup.sh.erb'),
   owner => 'root',
